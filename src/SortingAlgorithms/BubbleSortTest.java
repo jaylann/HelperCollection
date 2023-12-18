8847413ffbfbd7d55db06eae21efaa2578bcd0cb
MaxiSDev <max.03.strigl@hotmail.com>
2023-12-18 17:01:51 +0100

    Bubble Sort Test

---
+++ src/SortingAlgorithms/BubbleSortTest.java
@@ -0,0 +1,61 @@
+package SortingAlgorithms;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+import org.junit.jupiter.api.Test;
+
+public class BubbleSortTest {
+    @Test
+    public void testBubbleSort() {
+        // Create a list of integers
+        List<Integer> list = Arrays.asList(5, 3, 8, 2, 9, 1);
+
+        // Get the BubbleSort algorithm
+        Consumer<List<Integer>> bubbleSort = BubbleSort.getBubbleSortAlgorithm();
+
+        // Sort the list
+        bubbleSort.accept(list);
+
+        // Check if the list is sorted in ascending order
+        for (int i = 0; i < list.size() - 1; i++) {
+            assertTrue(list.get(i) <= list.get(i + 1));
+        }
+    }
+
+    @Test
+    public void testBubbleSortEmpty() {
+        // Create a list of integers
+        List<Integer> list = Arrays.asList();
+
+        // Get the BubbleSort algorithm
+        Consumer<List<Integer>> bubbleSort = BubbleSort.getBubbleSortAlgorithm();
+
+        // Sort the list
+        bubbleSort.accept(list);
+
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testBubbleSortSorted() {
+        // Create a list of integers
+        List<Integer> list = Arrays.asList(1, 2, 3, 5, 8, 9);
+
+        // Get the BubbleSort algorithm
+        Consumer<List<Integer>> bubbleSort = BubbleSort.getBubbleSortAlgorithm();
+
+        // Sort the list
+        bubbleSort.accept(list);
+
+        // Check if the list is sorted in ascending order
+        for (int i = 0; i < list.size() - 1; i++) {
+            assertTrue(list.get(i) <= list.get(i + 1));
+        }
+    }
+}
